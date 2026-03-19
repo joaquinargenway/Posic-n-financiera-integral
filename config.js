45e47589bb8f2764b34e22d156d51a6cf695102d
joaquinargenway <joaquin.gajardo@argenway.com>
2026-03-19 17:13:45 -0300

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 // Este archivo NO se actualiza cuando recibís un nuevo index.html
 // ══════════════════════════════════════════════════════════════════════════════
 
-window.CAJAS_BIN_ID   = 'TU_BIN_ID_AQUI';    // Bin ID de JSONbin
-window.CAJAS_API_KEY  = 'TU_API_KEY_AQUI';    // API Key de JSONbin
-window.CAJAS_ADMIN_PW = 'cajas2026';           // Contraseña admin (podés editar datos)
-window.CAJAS_VIEW_PW  = 'ver2026';             // Contraseña lectores (solo visualización)
+window.CAJAS_BIN_ID   = '69bc1ee3b7ec241ddc82cea5';    // Bin ID de JSONbin
+window.CAJAS_API_KEY  = '$2a$10$SiLLJfj17o5SXmbA8rhlFe0bLndFMrwdRII7hqzwEnwI6/LqGa/dG';    // API Key de JSONbin
+window.CAJAS_ADMIN_PW = 'vp7Agm98';           // Contraseña admin (podés editar datos)
+window.CAJAS_VIEW_PW  = 'Bariloch3!';             // Contraseña lectores (solo visualización)
